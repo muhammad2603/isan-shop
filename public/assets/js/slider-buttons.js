486fd103c97b1749bb736b504072b9c5690ce495
Muhammad Fattahillah. Mz <fattahillahmuhammad48@gmail.com>
2026-01-16 20:56:28 +0700

    Menyelesaikan aksi pada tombol previous

--- public/assets/js/slider-buttons.js
+++ public/assets/js/slider-buttons.js
@@ -19,13 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Inisialisasi calcMove untuk digunakan saat menentukan nilai pergeseran slider
     let calcMove = 0;
+    // Inisialisasi array untuk nilai-nilai pergeseran sebelumnya (saat next ditekan) agar memudahkan pergeseran item tanpa mengkalkulasi ulang
+    let previousCalc = [];
 
     // Event Listener: Saat tombol next di-klik
     nextBtn.addEventListener('click', () => {
         // Validasi jika item yang sudah dilewati telah mencapai total item dalam slider, untuk mencegah pergeseran tetap berlanjut saat mencapai batas akhir
         if (itemCardYangTelahTerlewati === sliderItems.length) return;
-
+        // Note: angka 8 ditentukan karena nilai awalnya adalah 8, saat tombol next ditekan, baris ini langsung dieksekusi
+        // Jika item card yang sudah dilewati sama dengan 0, maka tampilkan tombol previous
         if (itemCardYangTelahTerlewati === 8) prevBtn.classList.remove('hidden')
+        // Masukkan nilai saat ini (sebelum calcMove berubah) agar bisa digunakan saat user kembali ke-kategori sebelumnya
+        previousCalc.push(calcMove)
         // Validasi apakah total item dalam slider - item yang sudah dilewati adalah lebih besar dari 8? Digunakan untuk mengetahui apakah item bisa bergeser sebanyak item yang ditampilkan satu slider
         if (sliderItems.length - itemCardYangTelahTerlewati > 8) {
             // Jika benar:
@@ -57,26 +62,16 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     prevBtn.addEventListener('click', () => {
-
-        itemCardYangTelahTerlewati -= 8;
-
-        if (itemCardYangTelahTerlewati - 8 === 0) calcMove = 0;
-
-        if (itemCardYangTelahTerlewati % 8 < 8) {
-            // Kalkulasi pergeseran slider
-            /*
-             * Info:
-             * 20: adalah nilai gap antar item
-             * 1: adalah berapa banyak gap yang juga memakan ruang (koreksi lagi demi mencapai hasil yang baik)
-             */
-            calcMove += (sliderItemWidth + (20 * 1)) * (sliderItems.length - itemCardYangTelahTerlewati);
-            // Perbarui item yang sudah dilewati dengan menambahkan sisa itemnya
-            itemCardYangTelahTerlewati += sliderItems.length % 8;
-        }
-
-        // Geser slider ke-kiri untuk menampilkan kategori selanjutnya, baik sebanyak item yang tampil satu slider (full) maupun sisa item
-        slider.style.left = `-${calcMove}px`;
-
+        // Saat menyentuh item card terakhir dan setelahnya user menekan tombol prev, tampilkan tombol next
+        if (itemCardYangTelahTerlewati === sliderItems.length) nextBtn.classList.remove('hidden')
+        // Jika item yang dilewati dimodulus 8 sama dengan 0, maka kurangi nilai-nya dengan 8
+        if (itemCardYangTelahTerlewati % 8 === 0) itemCardYangTelahTerlewati -= 8
+        // Jika hasilnya bukan 0, maka kurangi dengan sisa item card terakhir
+        else itemCardYangTelahTerlewati -= sliderItems.length % 8
+        // Jika slider kembali ke-paling awal, maka hilangkan tombol previous
+        if (itemCardYangTelahTerlewati === 8) prevBtn.classList.add('hidden')
+        // Geser slide menggunakan nilai pergeseran sebelumnya yang disimpan dalam array previousCalc
+        slider.style.left = `-${previousCalc.pop()}px`;
     })
 
 })
